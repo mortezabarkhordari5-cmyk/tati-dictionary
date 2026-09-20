caa118cd953f21922b3c69d0cba2f9198b9b0c79
mortezabarkhordari5-cmyk <mortezabarkhordari5@gmail.com>
2026-09-20 08:28:35 +0330

    Update sw.js
    self.addEventListener('fetch', () => {});

--- sw.js
+++ sw.js
@@ -1,15 +1,1 @@
-const CACHE_NAME = 'tati-dict-v1';
-
-self.addEventListener('install', (event) => {
-  self.skipWaiting();
-});
-
-self.addEventListener('activate', (event) => {
-  event.waitUntil(clients.claim());
-});
-
-self.addEventListener('fetch', (event) => {
-  event.respondWith(
-    fetch(event.request).catch(() => caches.match(event.request))
-  );
-});
+self.addEventListener('fetch', () => {});
